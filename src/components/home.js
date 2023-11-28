e0bc1fc43a5dbdfe1a56d78b0ed2dd947318b51d
confidentmeerkat <confidentmeerkat@gmail.com>
2023-11-29 04:15:42 +0900

    change buyer name placeholder

--- src/components/home.js
+++ src/components/home.js
@@ -82,7 +82,7 @@ const Home = () => {
                 type="text"
                 name="name"
                 id="name"
-                placeholder="Jhon Doe"
+                placeholder="Jhon Smith"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={name}
                 onChange={handleChange(setName)}
@@ -101,7 +101,7 @@ const Home = () => {
                 type="email"
                 name="email"
                 id="email"
-                placeholder="jhondoe@gmail.com"
+                placeholder="jhon.smith@gmail.com"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={email}
                 onChange={handleChange(setEmail)}
